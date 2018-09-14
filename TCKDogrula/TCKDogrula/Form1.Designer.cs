09e4abd69c38a099667a007e707311d5fb69f1c1
Berkay <berkayyalcin7@icloud.com>
2018-09-15 01:44:00 +0300

    TC Kimlik No Doğrulama Web Service
    TC Kimlik No' Doğrulama ->
    
    Framework -> Bunifu Araçları Kullanılmıştır

---
+++ TCKDogrula/TCKDogrula/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace TCKDogrula
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblOnay = new System.Windows.Forms.Label();
+            this.lblHata = new System.Windows.Forms.Label();
+            this.txtDogumYil = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txtSoyad = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txtAd = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.txtTCK = new Bunifu.Framework.UI.BunifuMaterialTextbox();
+            this.bunifuCustomLabel5 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel4 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel3 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.bunifuCustomLabel2 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.bunifuFlatButton1 = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("X-Files", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(101, 25);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(386, 17);
+            this.bunifuCustomLabel1.TabIndex = 0;
+            this.bunifuCustomLabel1.Text = "T.C Kimlik Numarası Doğrulama Sistemi";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblOnay);
+            this.groupBox1.Controls.Add(this.lblHata);
+            this.groupBox1.Controls.Add(this.bunifuFlatButton1);
+            this.groupBox1.Controls.Add(this.txtDogumYil);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.txtTCK);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel5);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel4);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel3);
+            this.groupBox1.Controls.Add(this.bunifuCustomLabel2);
+            this.groupBox1.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Black;
+            this.groupBox1.Location = new System.Drawing.Point(104, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 258);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Bilgileriniz";
+            // 
+            // lblOnay
+            // 
+            this.lblOnay.AutoSize = true;
+            this.lblOnay.Font = new System.Drawing.Font("Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOnay.ForeColor = System.Drawing.Color.DarkGreen;
+            this.lblOnay.Location = new System.Drawing.Point(6, 218);
+            this.lblOnay.Name = "lblOnay";
+            this.lblOnay.Size = new System.Drawing.Size(0, 23);
+            this.lblOnay.TabIndex = 10;
+            // 
+            // lblHata
+            // 
+            this.lblHata.AutoSize = true;
+            this.lblHata.Font = new System.Drawing.Font("Century Schoolbook", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblHata.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblHata.Location = new System.Drawing.Point(6, 218);
+            this.lblHata.Name = "lblHata";
+            this.lblHata.Size = new System.Drawing.Size(0, 23);
+            this.lblHata.TabIndex = 9;
+            // 
+            // txtDogumYil
+            // 
+            this.txtDogumYil.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtDogumYil.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtDogumYil.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtDogumYil.HintForeColor = System.Drawing.Color.Empty;
+            this.txtDogumYil.HintText = "";
+            this.txtDogumYil.isPassword = false;
+            this.txtDogumYil.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txtDogumYil.LineIdleColor = System.Drawing.Color.DarkSlateGray;
+            this.txtDogumYil.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txtDogumYil.LineThickness = 3;
+            this.txtDogumYil.Location = new System.Drawing.Point(141, 167);
+            this.txtDogumYil.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDogumYil.Name = "txtDogumYil";
+            this.txtDogumYil.Size = new System.Drawing.Size(263, 33);
+            this.txtDogumYil.TabIndex = 7;
+            this.txtDogumYil.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtDogumYil.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDogumYil_KeyPress_1);
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtSoyad.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtSoyad.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtSoyad.HintForeColor = System.Drawing.Color.Empty;
+            this.txtSoyad.HintText = "";
+            this.txtSoyad.isPassword = false;
+            this.txtSoyad.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txtSoyad.LineIdleColor = System.Drawing.Color.DarkSlateGray;
+            this.txtSoyad.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txtSoyad.LineThickness = 3;
+            this.txtSoyad.Location = new System.Drawing.Point(141, 123);
+            this.txtSoyad.Margin = new System.Windows.Forms.Padding(4);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(263, 33);
+            this.txtSoyad.TabIndex = 6;
+            this.txtSoyad.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtSoyad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoyad_KeyPress);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtAd.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtAd.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtAd.HintForeColor = System.Drawing.Color.Empty;
+            this.txtAd.HintText = "";
+            this.txtAd.isPassword = false;
+            this.txtAd.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txtAd.LineIdleColor = System.Drawing.Color.DarkSlateGray;
+            this.txtAd.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txtAd.LineThickness = 3;
+            this.txtAd.Location = new System.Drawing.Point(141, 72);
+            this.txtAd.Margin = new System.Windows.Forms.Padding(4);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(263, 33);
+            this.txtAd.TabIndex = 5;
+            this.txtAd.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtAd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAd_KeyPress);
+            // 
+            // txtTCK
+            // 
+            this.txtTCK.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtTCK.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.txtTCK.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtTCK.HintForeColor = System.Drawing.Color.Empty;
+            this.txtTCK.HintText = "";
+            this.txtTCK.isPassword = false;
+            this.txtTCK.LineFocusedColor = System.Drawing.Color.Blue;
+            this.txtTCK.LineIdleColor = System.Drawing.Color.DarkSlateGray;
+            this.txtTCK.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.txtTCK.LineThickness = 3;
+            this.txtTCK.Location = new System.Drawing.Point(141, 27);
+            this.txtTCK.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTCK.Name = "txtTCK";
+            this.txtTCK.Size = new System.Drawing.Size(263, 33);
+            this.txtTCK.TabIndex = 4;
+            this.txtTCK.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.txtTCK.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTCK_KeyPress);
+            // 
+            // bunifuCustomLabel5
+            // 
+            this.bunifuCustomLabel5.AutoSize = true;
+            this.bunifuCustomLabel5.Location = new System.Drawing.Point(6, 181);
+            this.bunifuCustomLabel5.Name = "bunifuCustomLabel5";
+            this.bunifuCustomLabel5.Size = new System.Drawing.Size(128, 19);
+            this.bunifuCustomLabel5.TabIndex = 3;
+            this.bunifuCustomLabel5.Text = "DOĞUM YILI :";
+            this.bunifuCustomLabel5.Click += new System.EventHandler(this.bunifuCustomLabel5_Click);
+            // 
+            // bunifuCustomLabel4
+            // 
+            this.bunifuCustomLabel4.AutoSize = true;
+            this.bunifuCustomLabel4.Location = new System.Drawing.Point(6, 137);
+            this.bunifuCustomLabel4.Name = "bunifuCustomLabel4";
+            this.bunifuCustomLabel4.Size = new System.Drawing.Size(81, 19);
+            this.bunifuCustomLabel4.TabIndex = 2;
+            this.bunifuCustomLabel4.Text = "Soyad :";
+            // 
+            // bunifuCustomLabel3
+            // 
+            this.bunifuCustomLabel3.AutoSize = true;
+            this.bunifuCustomLabel3.Location = new System.Drawing.Point(6, 86);
+            this.bunifuCustomLabel3.Name = "bunifuCustomLabel3";
+            this.bunifuCustomLabel3.Size = new System.Drawing.Size(70, 19);
+            this.bunifuCustomLabel3.TabIndex = 1;
+            this.bunifuCustomLabel3.Text = "Ad     :";
+            // 
+            // bunifuCustomLabel2
+            // 
+            this.bunifuCustomLabel2.AutoSize = true;
+            this.bunifuCustomLabel2.Font = new System.Drawing.Font("Castellar", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel2.Location = new System.Drawing.Point(6, 41);
+            this.bunifuCustomLabel2.Name = "bunifuCustomLabel2";
+            this.bunifuCustomLabel2.Size = new System.Drawing.Size(81, 19);
+            this.bunifuCustomLabel2.TabIndex = 0;
+            this.bunifuCustomLabel2.Text = "T.C No :";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(98, 328);
+            this.panel1.TabIndex = 3;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TCKDogrula.Properties.Resources.close_red;
+            this.pictureBox2.Location = new System.Drawing.Point(490, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(42, 33);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 63);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // bunifuFlatButton1
+            // 
+            this.bunifuFlatButton1.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuFlatButton1.BorderRadius = 0;
+            this.bunifuFlatButton1.ButtonText = "Arat";
+            this.bunifuFlatButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bunifuFlatButton1.DisabledColor = System.Drawing.Color.Gray;
+            this.bunifuFlatButton1.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bunifuFlatButton1.Iconcolor = System.Drawing.Color.Transparent;
+            this.bunifuFlatButton1.Iconimage = ((System.Drawing.Image)(resources.GetObject("bunifuFlatButton1.Iconimage")));
+            this.bunifuFlatButton1.Iconimage_right = null;
+            this.bunifuFlatButton1.Iconimage_right_Selected = null;
+            this.bunifuFlatButton1.Iconimage_Selected = null;
+            this.bunifuFlatButton1.IconMarginLeft = 0;
+            this.bunifuFlatButton1.IconMarginRight = 0;
+            this.bunifuFlatButton1.IconRightVisible = true;
+            this.bunifuFlatButton1.IconRightZoom = 0D;
+            this.bunifuFlatButton1.IconVisible = true;
+            this.bunifuFlatButton1.IconZoom = 90D;
+            this.bunifuFlatButton1.IsTab = false;
+            this.bunifuFlatButton1.Location = new System.Drawing.Point(309, 212);
+            this.bunifuFlatButton1.Margin = new System.Windows.Forms.Padding(6, 4, 6, 4);
+            this.bunifuFlatButton1.Name = "bunifuFlatButton1";
+            this.bunifuFlatButton1.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.bunifuFlatButton1.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.bunifuFlatButton1.OnHoverTextColor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.selected = false;
+            this.bunifuFlatButton1.Size = new System.Drawing.Size(95, 36);
+            this.bunifuFlatButton1.TabIndex = 8;
+            this.bunifuFlatButton1.Text = "Arat";
+            this.bunifuFlatButton1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.bunifuFlatButton1.Textcolor = System.Drawing.Color.White;
+            this.bunifuFlatButton1.TextFont = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bunifuFlatButton1.Click += new System.EventHandler(this.bunifuFlatButton1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(547, 325);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtSoyad;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtAd;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtTCK;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel5;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel4;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel3;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblOnay;
+        private System.Windows.Forms.Label lblHata;
+        private Bunifu.Framework.UI.BunifuFlatButton bunifuFlatButton1;
+        private Bunifu.Framework.UI.BunifuMaterialTextbox txtDogumYil;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ TCKDogrula/TCKDogrula/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TCKDogrula.TCKDogrulaServis;
+
+namespace TCKDogrula
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bunifuCustomLabel5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //Ad-Soyad - Rakam Alamaz 
+        //TC-No ve Doğum-Yılı Harf Alamaz.
+        #region Harf-Sayı Kontrolü
+
+        private void txtTCK_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtAd_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                                                  && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void txtSoyad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                                                  && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void txtDogumYil_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        #endregion
+
+
+        private void bunifuFlatButton1_Click(object sender, EventArgs e)
+        {
+            KPSPublicSoapClient tckServis=new KPSPublicSoapClient();
+            while (true)
+            {
+                try
+                {
+                    bool eslesme = tckServis.TCKimlikNoDogrula(Convert.ToInt64(txtTCK.Text), txtAd.Text.ToUpper().TrimEnd(),
+                        txtSoyad.Text.ToUpper().TrimEnd(), Convert.ToInt32(txtDogumYil.Text));
+                    lblOnay.Text = "";
+                    lblHata.Text = "";
+
+                    if (Convert.ToInt64(txtTCK.Text) % 2 != 0)
+                    {
+                        lblHata.Text = "TC No Tek Sayı ile Bitemez";
+                        break;
+                    }
+                  
+                    if (eslesme)
+                    {
+                        lblOnay.Text = "Kimlik Eşleşmesi Sağlandı";
+                        break;
+                    }
+
+                    else
+                    {
+                        lblHata.Text = "Hata Eşleşme Sağlanamadı";
+                        break;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Hata Algılandı ! Lütfen Tekrar Deneyin", "UYARI", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                   break;
+
+                }
+
+            }
+            
+            
+
+            
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            DialogResult secenek = MessageBox.Show("Çıkış yapılsın mı ?", "Bilgilendirme",
+                MessageBoxButtons.YesNoCancel, MessageBoxIcon.Warning);
+            if (secenek == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+            
+        }
+
+        
+    }
+}
